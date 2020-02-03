5f4a6f8b0b5c599579437cbc90e2a8750d79ab3f
funkyhippo <52957110+funkyhippo@users.noreply.github.com>
2020-02-03 09:57:50 -0800

    Prod fixes.

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 # Installation
 
-Change `BASE_URL` to the proxy's base, without a leading slash.
-
-```javascript
-const BASE_URL = "https://example.com";
-```
-
-Next, go to your Chrome [extensions](chrome://extensions) page and enable developer mode (at the top right). That should reveal three new options, `Load unpacked`, `Pack extension`, and `Update`.
+Go to your Chrome [extensions](chrome://extensions) page and enable developer mode (at the top right). That should reveal three new options, `Load unpacked`, `Pack extension`, and `Update`.
 
 
 Select `Load unpacked`, and select the folder for this repo. You should see a Guya icon.
--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-const BASE_URL = "";
+const BASE_URL = "https://guya.moe";
 
 const ctx = chrome || browser;
 
 let updateIcon = tabId => {
-  try {
-    ctx.tabs.get(tabId, tab => {
-      if (
-        tab.url.includes("mangadex.org/title") ||
-        tab.url.includes("mangadex.org/manga") ||
-        tab.url.includes("mangadex.org/chapter") ||
-        tab.url.includes("nhentai.net/g")
-      ) {
-        ctx.browserAction.setIcon({ path: "logo_small.png" });
-      } else {
+  ctx.tabs.get(tabId, tab => {
+    if (!ctx.runtime.lastError) {
+      try {
+        if (
+          tab.url.includes("mangadex.org/title") ||
+          tab.url.includes("mangadex.org/manga") ||
+          tab.url.includes("mangadex.org/chapter") ||
+          tab.url.includes("nhentai.net/g")
+        ) {
+          ctx.browserAction.setIcon({ path: "logo_small.png" });
+        } else {
+          ctx.browserAction.setIcon({ path: "logo_small_bw.png" });
+        }
+      } catch (e) {
         ctx.browserAction.setIcon({ path: "logo_small_bw.png" });
       }
-    });
-  } catch (e) {
-    ctx.browserAction.setIcon({ path: "logo_small_bw.png" });
-  }
+    }
+  });
 };
 
 ctx.browserAction.onClicked.addListener(tab => {
